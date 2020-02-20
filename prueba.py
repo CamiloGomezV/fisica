60f425ea3c43e3563e799428be2db33871da4874
Camilo Gomez <gomezvargas38@gmail.com>
2020-02-20 17:31:55 -0500

    ultima version

--- prueba.py
+++ prueba.py
@@ -22,3 +22,30 @@ ani = FuncAnimation(fig, update, frames=np.linspace(0, 2*np.pi, 128),
 print(xdata)
 print(ydata)
 plt.show()
+
+"""
+def set_animation(self):
+        # plot setup: axis, labels, title, grid, etc.
+        self.fig = plt.figure()
+        self.ax = plt.axes(autoscale_on = False, xlim = (0, 26), ylim = (0, 15))
+        self.ax.set(xlabel = 'x', ylabel = 'y', title = 'Simulation')
+        self.ax.grid()
+        # line points setup, time template, points on top of axes
+        self.point = [self.ax.plot([], [], 'o-', c = self.cls[i], lw = 0)for i in range(self.number)]
+        self.line = [self.ax.plot([], [], ls = "--", lw = 2, c = self.cls[i]) for i in range(self.number)]
+        return
+
+    def init(self):
+        # function used to draw a clear frame
+        return
+
+    def animate(self, idx):
+        # function to call at each frame
+        for i in range(self.number):
+            a = self.artists[i]
+            self.xdata, self.ydata = a.get_trajectory()
+            self.line[i][0].set_data([self.xdata[i]], [self.ydata[i]])
+            if self.ydata[idx] >= 0.:
+                self.point[i][0].set_data([self.xdata[idx]], [self.ydata[idx]])
+
+"""
